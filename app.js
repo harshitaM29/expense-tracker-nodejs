af33d860dc3afd1d3833925f801b913e804bd6ed
harshitaM29 <harshita.mundhe29@gmail.com>
2023-08-09 09:00:13 +0530

    added basic routes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const app = require('express');
-
+const express = require('express');
+const sequelize = require('./utils/database');
 const bodyParser = require('body-parser');
 
+const app = express();
+const cors = require('cors');
 
+app.use(cors());
+
+const expensesRoutes = require('./routes/expenses');
+app.use(bodyParser.json({extended:false}));
+
+app.use(expensesRoutes);
 
 app.listen(4000);
---
+++ routes/expenses.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/expenses',);
+
+router.post('/expenses');
+
+module.exports = router;
